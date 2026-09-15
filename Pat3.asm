f1b4b301b5b2b88e8c4632dd508cecb7960b16df
Md. Miraz Morshed <miraz.rzsian21@gmail.com>
2026-09-15 23:26:11 +0600

    Add pyramid pattern printing in Pat3.asm
    Implement a program to print a pyramid pattern based on user input.

---
+++ Pat3.asm
@@ -0,0 +1,49 @@
+include 'EMU8086.inc'
+.Model
+.Stack 100h
+.data
+    
+.code
+    main proc
+
+    mov ax, @data
+    mov ds,ax  
+    
+    ;               *
+    ;              ***
+    ;             *****
+    ;            *******
+    ;           *********
+
+    mov ah,1h
+    int 21h
+    sub al,'0'
+    mov ah,0h
+    mov cx,ax 
+    printn  
+    
+    mov ax,1h
+    outer:
+        push cx
+        dec cx
+        inner1: 
+            print " "
+            loop inner1 
+        mov cx,ax
+        add cx,cx
+        dec cx   ;cx=2*cx-1
+        inner2: 
+            print "*"
+            loop inner2
+        printn
+        inc ax
+        pop cx
+        loop outer
+        
+    
+        
+    
+
+
+    main endp
+end main
